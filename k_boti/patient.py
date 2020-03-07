e4b2683a4512f36d4b1a15221f9583882cf80a72
Botond Kurdi <botond.kurdi@lhsystems.com>
2020-03-07 18:37:42 +0100

    refactoring
    ----------------------
    branch [[k_boti]]

--- k_boti/patient.py
+++ k_boti/patient.py
@@ -1,14 +1,33 @@
+from k_boti.pickle_creator import get_diastole_and_systole_frame, get_images_by_position, get_image_positions
 
 
 class Patient:
 
-    def __init__(self):
-        self.dia_images = []
-        self.sys_images = []
-        self.contours = []
-        self.study_id = 0
+    def __init__(self, contour_reader, dicom_reader):
+        self.contours = contour_reader.get_hierarchical_contours()
         self.age = 0
-        self.height = 0
-        self.weight = 0
-        self.gender = None
+
+        # separate diastole and systole by the first available frame
+        contour_a = None
+        contour_b = None
+        a_frame, b_frame = 0, 0
+        for slc in self.contours:
+            if len(self.contours[slc]) > 1:
+                a_frame, b_frame = self.contours[slc]
+                contour_a = self.contours[slc][a_frame]["lp"].tolist()
+                contour_b = self.contours[slc][b_frame]["lp"].tolist()
+
+        dia_frame, sys_frame = get_diastole_and_systole_frame(contour_a, contour_b, a_frame, b_frame)
+
+        # set diastole images
+        self.dia_images = get_images_by_position(dicom_reader, get_image_positions(self.contours, dia_frame))
+        self.sys_images = get_images_by_position(dicom_reader, get_image_positions(self.contours, sys_frame))
+        self.study_id = contour_reader.volume_data[0].value()
+        self.patient_weight = contour_reader.volume_data[4].value()
+        self.Patient_height = contour_reader.volume_data[5].value()
+        self.Patient_gender = contour_reader.volume_data[7].value()
+
+
+
+
 
