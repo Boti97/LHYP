88ee858c6c25510b1f0b33b0475fa9ebc4d26831
boti97 <kurdi.boti@gmail.com>
2020-05-10 17:35:38 +0200

    lists now completed with 0-s, not trimmed

--- k_boti/network.py
+++ k_boti/network.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets
-import torch.nn.functional as f
+import torch.nn.functional as torch_fun
 
 from patient import Patient
 from neuralinput import NeuralInput
@@ -39,27 +39,6 @@ class SimpleClassifier(nn.Module):
         return out
 
 
-def get_wall_thicknesses(patients):
-    all_wall_thicknesses = []
-    min_wall_thickness_list_length = 1000
-    for patient in patients:
-        if len(patient.wall_thicknesses[0]) == 0:
-            print("Need to be deleted: " + patient.patiend_id)
-        else:
-            if len(patient.wall_thicknesses[0]) < min_wall_thickness_list_length:
-                min_wall_thickness_list_length = len(patient.wall_thicknesses[0])
-            all_wall_thicknesses.append(patient.wall_thicknesses)
-
-    # trimming all the lists to the same length
-    all_wall_thicknesses = torch.Tensor(
-        get_trimmed_wall_thicknesses(all_wall_thicknesses, min_wall_thickness_list_length))
-
-    # flattening
-    all_wall_thicknesses = all_wall_thicknesses.view(-1, 4 * min_wall_thickness_list_length)
-
-    return all_wall_thicknesses
-
-
 def get_all_diagnoses(patients):
     all_diagnoses = []
     for patient in patients:
@@ -67,20 +46,41 @@ def get_all_diagnoses(patients):
     return all_diagnoses
 
 
-def get_distances(patients):
-    all_distances = []
-    min_length_of_list = 1000
+def get_wall_thicknesses(patients):
+    all_wall_thicknesses = []
+    max_wall_thickness_list_length = -1
     for patient in patients:
-        for d in patient.distances:
-            if len(d) < min_length_of_list:
-                min_length_of_list = len(d)
-        all_distances.append(patient.distances)
+        if len(patient.wall_thicknesses[0]) == 0:
+            print("Need to be deleted: " + patient.patiend_id)
+        else:
+            if len(patient.wall_thicknesses[0]) > max_wall_thickness_list_length:
+                max_wall_thickness_list_length = len(patient.wall_thicknesses[0])
+            all_wall_thicknesses.append(patient.wall_thicknesses)
 
-    # trimming all the lists to the same length
-    all_distances = torch.Tensor(get_trimmed_distances(all_distances, min_length_of_list))
+    # completing all the lists to the same length
+    all_wall_thicknesses = torch.Tensor(
+        get_completed_wall_thicknesses(all_wall_thicknesses, max_wall_thickness_list_length))
 
     # flattening
-    all_distances = all_distances.view(-1, 2 * min_length_of_list).squeeze(1)
+    all_wall_thicknesses = all_wall_thicknesses.view(-1, 4 * max_wall_thickness_list_length)
+
+    return all_wall_thicknesses
+
+
+def get_distances(patients):
+    all_distances = []
+    max_length_of_list = -1
+    for patient in patients:
+        for d in patient.distances:
+            if len(d) > max_length_of_list:
+                max_length_of_list = len(d)
+        all_distances.append(patient.distances)
+
+    # completing all the lists to the same length
+    all_distances = torch.Tensor(get_completed_distances(all_distances, max_length_of_list))
+
+    # flattening
+    all_distances = all_distances.view(-1, 2 * max_length_of_list).squeeze(1)
 
     return all_distances
 
@@ -88,32 +88,33 @@ def get_distances(patients):
 def get_polygons(patients):
     all_polygons_ln = []
     all_polygons_lp = []
-    min_length_of_list_ln = 1000
-    min_length_of_list_lp = 1000
+    max_length_of_list_ln = -1
+    max_length_of_list_lp = -1
     for patient in patients:
-        if len(patient.ln_polygons) < min_length_of_list_ln:
-            min_length_of_list_ln = len(patient.ln_polygons)
-        if len(patient.lp_polygons) < min_length_of_list_lp:
-            min_length_of_list_lp = len(patient.lp_polygons)
+        if len(patient.ln_polygons) > max_length_of_list_ln:
+            max_length_of_list_ln = len(patient.ln_polygons)
+        if len(patient.lp_polygons) > max_length_of_list_lp:
+            max_length_of_list_lp = len(patient.lp_polygons)
         all_polygons_ln.append(patient.ln_polygons)
         all_polygons_lp.append(patient.lp_polygons)
 
-    if min_length_of_list_ln > min_length_of_list_lp:
-        min_length_of_list_ln = min_length_of_list_lp
+    # the two length should be the same for concatenating
+    if max_length_of_list_ln < max_length_of_list_lp:
+        max_length_of_list_ln = max_length_of_list_lp
     else:
-        min_length_of_list_lp = min_length_of_list_ln
+        max_length_of_list_lp = max_length_of_list_ln
 
-    # trimming all the lists to the same length
-    all_polygons_ln_tensor = torch.Tensor(get_trimmed_polygons(all_polygons_ln, min_length_of_list_ln))
-    all_polygons_lp_tensor = torch.Tensor(get_trimmed_polygons(all_polygons_lp, min_length_of_list_lp))
+    # completed all the lists to the same length
+    all_polygons_ln_tensor = torch.Tensor(get_completed_polygons(all_polygons_ln, max_length_of_list_ln))
+    all_polygons_lp_tensor = torch.Tensor(get_completed_polygons(all_polygons_lp, max_length_of_list_lp))
 
     # flattening
-    trimmed_polygons_ln = all_polygons_ln_tensor.view(-1, min_length_of_list_ln * 4 * 2).squeeze(1)
-    trimmed_polygons_lp = all_polygons_lp_tensor.view(-1, min_length_of_list_lp * 4 * 2).squeeze(1)
+    completed_polygons_ln = all_polygons_ln_tensor.view(-1, max_length_of_list_ln * 4 * 2).squeeze(1)
+    completed_polygons_lp = all_polygons_lp_tensor.view(-1, max_length_of_list_lp * 4 * 2).squeeze(1)
 
-    all_trimmed_polygons = torch.cat((trimmed_polygons_ln, trimmed_polygons_lp), 1)
+    all_competed_polygons = torch.cat((completed_polygons_ln, completed_polygons_lp), 1)
 
-    return all_trimmed_polygons
+    return all_competed_polygons
 
 
 def diagnoses_converter(all_diagnoses):
@@ -135,42 +136,53 @@ def diagnoses_converter(all_diagnoses):
     return diagnoses_converted
 
 
-def get_trimmed_wall_thicknesses(all_wall_thicknesses, min_wall_thickness_number):
-    trimmed_wall_thicknesses = []
+def get_completed_wall_thicknesses(all_wall_thicknesses, max_wall_thickness_length):
+    completed_wall_thicknesses = []
     for wall_thicknesses in all_wall_thicknesses:
         patient_wall_thicknesses = []
-        for w in wall_thicknesses:
-            if len(w) > min_wall_thickness_number:
-                patient_wall_thicknesses.append(w[:min_wall_thickness_number])
+        for wall_thickness in wall_thicknesses:
+            if len(wall_thickness) < max_wall_thickness_length:
+                wall_thickness += [0] * (max_wall_thickness_length - len(wall_thickness))
+                patient_wall_thicknesses.append(wall_thickness)
+            elif len(wall_thickness) == max_wall_thickness_length:
+                patient_wall_thicknesses.append(wall_thickness)
             else:
-                patient_wall_thicknesses.append(w)
+                print("There's a longer list. Finding the max wall thickness length was not successful.")
 
-        trimmed_wall_thicknesses.append(patient_wall_thicknesses)
-    return trimmed_wall_thicknesses
+        completed_wall_thicknesses.append(patient_wall_thicknesses)
+    return completed_wall_thicknesses
 
 
-def get_trimmed_distances(all_distances, min_distance_list_length):
-    trimmed_distances = []
+def get_completed_distances(all_distances, max_distance_list_length):
+    completed_distances = []
     for distances in all_distances:
         patient_distances = []
-        for w in distances:
-            if len(w) > min_distance_list_length:
-                patient_distances.append(w[:min_distance_list_length])
+        for distance in distances:
+            if len(distance) < max_distance_list_length:
+                distance += [0] * (max_distance_list_length - len(distance))
+                patient_distances.append(distance)
+            elif len(distance) == max_distance_list_length:
+                patient_distances.append(distance)
             else:
-                patient_distances.append(w)
+                print("There's a longer list. Finding the max distance length was not successful.")
 
-        trimmed_distances.append(patient_distances)
-    return trimmed_distances
+        completed_distances.append(patient_distances)
+    return completed_distances
 
 
-def get_trimmed_polygons(all_polygons, min_length_of_list):
-    trimmed_polygons = []
+def get_completed_polygons(all_polygons, max_length_of_list):
+    completed_polygons = []
     for polygon in all_polygons:
-        if len(polygon) > min_length_of_list:
-            trimmed_polygons.append(polygon[:min_length_of_list])
+        if len(polygon) < max_length_of_list:
+            for i in range((max_length_of_list - len(polygon))):
+                polygon.append([np.array([0, 0]), np.array([0, 0]), np.array([0, 0]), np.array([0, 0])])
+            # polygon += [[0,0],[0,0],[0,0],[0,0]] * (max_length_of_list - len(polygon))
+            completed_polygons.append(polygon)
+        elif len(polygon) == max_length_of_list:
+            completed_polygons.append(polygon)
         else:
-            trimmed_polygons.append(polygon)
-    return trimmed_polygons
+            print("There's a longer list. Finding the max polygon list length was not successful.")
+    return completed_polygons
 
 
 def train_neural_network(neural_inputs_pickles_path):
@@ -184,23 +196,33 @@ def train_neural_network(neural_inputs_pickles_path):
 
     # concatenate all the available data to one tensor as the input
     # concat_all_data_x = torch.cat((distances, wall_thicknesses, polygons), 1)
-    concat_all_data_x = wall_thicknesses
+
+    # concat_all_data_x = torch.cat((distances, polygons), 1)
+    # concat_all_data_x = torch.cat((distances, wall_thicknesses), 1)
+    # concat_all_data_x = torch.cat((polygons, wall_thicknesses), 1)
+
+    concat_all_data_x = distances
+    # concat_all_data_x = polygons
+    # concat_all_data_x = wall_thicknesses
 
     # normalize input
-    concat_all_data_x = f.normalize(concat_all_data_x)
+    concat_all_data_x = torch_fun.normalize(concat_all_data_x)
 
-    train_x = concat_all_data_x[:int(len(concat_all_data_x) * 0.6)].type(torch.float32)
-    test_x = concat_all_data_x[int(len(concat_all_data_x) * 0.6):].type(torch.float32)
-    train_y = all_data_y[:int(len(all_data_y) * 0.6)].type(torch.int64)
-    test_y = all_data_y[int(len(all_data_y) * 0.6):].type(torch.int64)
+    # set train, test and dev
+    train_x = concat_all_data_x[:int(len(concat_all_data_x) * 0.8)].type(torch.float32)
+    train_y = all_data_y[:int(len(all_data_y) * 0.8)].type(torch.int64)
 
-    all_idx = np.arange(len(train_x))
-    train_idx = all_idx[:round(len(all_idx) * 0.7)]
-    dev_idx = all_idx[round(len(all_idx) * 0.7):]
+    test_x = concat_all_data_x[int(len(concat_all_data_x) * 0.8):].type(torch.float32)
+    test_y = all_data_y[int(len(all_data_y) * 0.8):].type(torch.int64)
+
+    all__train_idx = np.arange(len(train_x))
+    train_idx = all__train_idx[:round(len(all__train_idx) * 0.8)]
+    dev_idx = all__train_idx[round(len(all__train_idx) * 0.8):]
     dev_x = train_x[dev_idx]
     dev_y = train_y[dev_idx]
     train_x = train_x[train_idx]
     train_y = train_y[train_idx]
+
     print("Train size:", train_x.size(), train_y.size())
     print("Dev size:", dev_x.size(), dev_y.size())
     print("Test size:", test_x.size(), train_y.size())
@@ -214,7 +236,7 @@ def train_neural_network(neural_inputs_pickles_path):
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters())
 
-    batch_size = 10
+    batch_size = 20
     train_iter = BatchedIterator(train_x, train_y, batch_size)
     dev_iter = BatchedIterator(dev_x, dev_y, batch_size)
     test_iter = BatchedIterator(test_x, test_y, batch_size)
@@ -224,7 +246,7 @@ def train_neural_network(neural_inputs_pickles_path):
     all_train_acc = []
     all_dev_acc = []
 
-    for epoch in range(10):
+    for epoch in range(200):
         # training loop
         for bi, (batch_x, batch_y) in enumerate(train_iter.iterate_once()):
             y_out = model(batch_x)
@@ -256,12 +278,12 @@ def train_neural_network(neural_inputs_pickles_path):
     print(test_acc)
 
     fig, (ax1, ax2) = plt.subplots(2)
-    ax1.plot(all_train_loss, label='train')
-    ax1.plot(all_dev_loss, label='dev')
+    ax1.plot(all_train_loss, label='train loss')
+    ax1.plot(all_dev_loss, label='dev loss')
     ax1.legend()
 
-    ax2.plot(all_train_acc, label='train')
-    ax2.plot(all_dev_acc, label='dev')
+    ax2.plot(all_train_acc, label='train acc')
+    ax2.plot(all_dev_acc, label='dev acc')
     ax2.legend()
     plt.show()
 
